686d32b7f84938b49a09ebf4022179fd2e681d92
Johannes Alkjær <jalkjaer@users.noreply.github.com>
2017-03-21 16:57:45 +0100

    faster startup and better shutdown handling
    respawn scheduler with signal handling for proper closing and avoid K8S CrashLoopBackOff stalls
    netcat checking using -z
    use curl build-in retrying at the cost of some logging

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -6,42 +6,66 @@ POSTGRES_HOST="postgres"
 POSTGRES_PORT="5432"
 RABBITMQ_HOST="rabbitmq"
 RABBITMQ_CREDS="airflow:airflow"
-FERNET_KEY=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print FERNET_KEY")
+FERNET_KEY=$(python -c "from cryptography.fernet import Fernet; print(Fernet.generate_key().decode())")
 
 # Generate Fernet key
 sed -i "s/{FERNET_KEY}/${FERNET_KEY}/" $AIRFLOW_HOME/airflow.cfg
 
 # wait for rabbitmq
 if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] || [ "$1" = "flower" ] ; then
-  j=0
-  while ! curl -sI -u $RABBITMQ_CREDS http://$RABBITMQ_HOST:15672/api/whoami |grep '200 OK'; do
-    j=`expr $j + 1`
-    if [ $j -ge $TRY_LOOP ]; then
-      echo "$(date) - $RABBITMQ_HOST still not reachable, giving up"
+  echo "$(date) - waiting for RabbitMQ..."
+  RABBIT_RESPONSE=$(curl -so /dev/null --connect-timeout 2 --retry 10 --retry-max-time 20 -w "%{http_code}" -u $RABBITMQ_CREDS http://$RABBITMQ_HOST:15672/api/whoami )
+  if [ "$RABBIT_RESPONSE" != "200" ] ; then
+      echo "$(date) - $RABBITMQ_HOST not reachable after 20 seconds of trying..."
       exit 1
-    fi
-    echo "$(date) - waiting for RabbitMQ... $j/$TRY_LOOP"
-    sleep 5
-  done
+  else
+     echo "$(date) - $RABBITMQ_HOST is up"
+  fi
 fi
 
 # wait for DB
 if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] ; then
   i=0
-  while ! nc $POSTGRES_HOST $POSTGRES_PORT >/dev/null 2>&1 < /dev/null; do
-    i=`expr $i + 1`
+  while ! nc -z -w 2 $POSTGRES_HOST $POSTGRES_PORT; do
+    echo "$(date) - Waiting for postgres"
+    i=$(($i + 1))
     if [ $i -ge $TRY_LOOP ]; then
       echo "$(date) - ${POSTGRES_HOST}:${POSTGRES_PORT} still not reachable, giving up"
       exit 1
     fi
     echo "$(date) - waiting for ${POSTGRES_HOST}:${POSTGRES_PORT}... $i/$TRY_LOOP"
-    sleep 5
+    sleep 1
   done
   if [ "$1" = "webserver" ]; then
     echo "Initialize database..."
     $CMD initdb
   fi
-  sleep 5
 fi
 
-exec $CMD "$@"
+_term() { 
+  echo "$(date) - entrypoint.sh: Recieved TERM/INT, forwarding to airflow"
+  RESPAWN=false
+  kill -TERM "$child" 
+}
+_quit() { 
+  echo "$(date) - entrypoint.sh: Recieved QUIT, forwarding to airflow"
+  RESPAWN=false
+  kill -QUIT "$child" 
+}
+
+
+if [ "$1" = "scheduler" ]; then 
+  RESPAWN=true
+  trap _term SIGTERM
+  trap _term SIGINT
+  trap _quit SIGQUIT
+  while [ "$RESPAWN" = true ] ; do
+    echo "$(date) - entrypoint.sh: Booting scheduler with respawn"
+    $CMD "$@" &
+    child=$!
+    wait "$child"
+  done
+else
+  echo "$(date) - entrypoint.sh: Starting $1"
+  exec $CMD "$@"
+fi
